3da252cdde55dd82c2b1d3e4afe2f91fd8153ada
Arif Hossain <hossaininfo@ymail.com>
2023-10-17 15:03:32 +0200

    Initial commit

---
+++ src/main/java/com/handel/handel_api/HandelApiApplication.java
@@ -0,0 +1,13 @@
+package com.handel.handel_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandelApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/handel/handel_api/HandelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handel.handel_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
